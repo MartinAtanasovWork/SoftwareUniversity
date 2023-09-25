54dc0cb8ebb09223359cae4a3f556333c9579188
MartinAtanasovWork <martiatanasof@gmail.com>
2023-09-25 18:59:25 +0300

    Update 02. English Name of the Last Digit.js

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit.js
@@ -0,0 +1,35 @@
+function solve(a){
+ let digit = a % 10;
+ switch(digit){
+    case 1:
+    console.log("one");
+    break;
+    case 2:
+    console.log("two");
+    case 3:
+    console.log("three");
+    break;   
+    case 4:
+    console.log("four");      
+    break;
+    case 5:
+    console.log("five");      
+    break;
+    case 6:
+    console.log("six");   
+    break;
+    case 7:
+    console.log("seven");
+    break;
+    case 8:
+    console.log("eight");
+    break;
+    case 9:
+    console.log("nine");
+    break;
+    case 0:
+    console.log("zero");
+    break;                    
+ }
+  }
+  solve(16)
