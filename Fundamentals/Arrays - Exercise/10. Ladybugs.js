1b13a0e6daf90c12c503ad4a1cedfcc48b537dfb
MartinAtanasovWork <martiatanasof@gmail.com>
2023-10-01 19:13:08 +0300

    Update 10. Ladybugs.js

--- Fundamentals/Arrays - Exercise/10. Ladybugs.js
+++ Fundamentals/Arrays - Exercise/10. Ladybugs.js
@@ -1,48 +1,39 @@
-function solve(command){
+function solve(command) {
     let size = command[0];
-    let indexes = command[1].split(" ");
-    let ladybugs = [];
-    for(let i = 0;i < size;i++){
-          ladybugs[i] = 0;  
-         
-    }   
-    for(let i = 0;i < indexes.length;i++){
-        ladybugs[Number(indexes[i])] = 1;
+    let indexes = command[1].split(" ").map(Number);
+    let ladybugs = Array(size).fill(0);
+  
+    // Place ladybugs on the field
+    for (let index of indexes) {
+      if (index >= 0 && index < size) {
+        ladybugs[index] = 1;
+      }
     }
-
-    for(let i =2;i < command.length;i++){
-        let tokens = command[i].split(" ");
-        let indexOfBug = Number(tokens[0]);
-        let moove = tokens[1];
-        let step = Number(tokens[2])
+  
+    for (let i = 2; i < command.length; i++) {
+      let tokens = command[i].split(" ");
+      let indexOfBug = Number(tokens[0]);
+      let move = tokens[1];
+      let step = Number(tokens[2]);
+  
+      if (ladybugs[indexOfBug] === 1) {
         ladybugs[indexOfBug] = 0;
-        if(moove == "right"){
-            for(let i = indexOfBug+step; i < size;i+= step){
-                if(i > ladybugs.length - 1){
-                    break;
-                }
-                if(ladybugs[i] == 0){
-                    ladybugs[i] = 1;
-                    break;
-                }
-            }
-        }else {
-            for(let i = indexOfBug - step; i > 0;i-= step){
-                if(i < 0){
-                    break;
-                }
-                if(ladybugs[i] == 0){
-                    ladybugs[i] = 1;
-                    break;
-                } 
+  
+        let newPosition = move === "right" ? indexOfBug + step : indexOfBug - step;
+  
+        while (newPosition >= 0 && newPosition < size) {
+          if (ladybugs[newPosition] === 0) {
+            ladybugs[newPosition] = 1;
+            break;
+          }
+          newPosition = move === "right" ? newPosition + step : newPosition - step;
         }
-                       
-    }
-       
+      }
     }
     console.log(ladybugs.join(" "));
-}
-solve([ 3, '0 1 2',
-'2 right 1']
-)
+  }
+  
+  solve([5, '0', '0 right 1', '1 right 2','3 right 1']);
+  
+
    
