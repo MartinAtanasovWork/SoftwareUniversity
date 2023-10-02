3d6b7267f149e912dbb50879e1d1b40b94be019f
MartinAtanasovWork <martiatanasof@gmail.com>
2023-10-02 15:32:48 +0300

    Create 01. Print N-th Element.js

---
+++ Fundamentals/Arrays - More Exercise/01. Print N-th Element.js
@@ -0,0 +1,9 @@
+function solve(arr){
+    let step = Number(arr[arr.length-1]);
+    let newArr = [];
+    for(let i = 0;i < arr.length - 1;i+=step){
+        newArr.push(arr[i]);
+    }
+    console.log(newArr.join(" "));
+}
+solve(['1', '2', '3', '4', '5', '6'])
