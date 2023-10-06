7ac304403c529b06eac1a07525d173b4b679571d
MartinAtanasovWork <martiatanasof@gmail.com>
2023-10-06 20:18:42 +0300

    Update 07. NxN Matrix.js

---
+++ Functions - Exercise/07. NxN Matrix.js
@@ -0,0 +1,10 @@
+function PrintMatrix(n){
+    for(let i = 0;i < n;i++){
+        let row = "";
+        for(let j=0;j < n;j++){
+            row += `${n} `;
+        }
+        console.log(row);
+    }
+}
+PrintMatrix(7)
