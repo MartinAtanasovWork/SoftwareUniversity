bb052ef13aeb9876b581641eef3b00f92b6df540
MartinAtanasovWork <martiatanasof@gmail.com>
2023-10-06 20:22:29 +0300

    Update 08. Perfect Number 1 ​.js

---
+++ Functions - Exercise/08. Perfect Number 1 ​.js
@@ -0,0 +1,10 @@
+function perfectNumber(number){
+    let sum = 0;
+    for(let i = 1;i < number;i++){
+        if(number % i == 0){
+            sum +=i;
+        }
+    }
+    console.log(sum == number ? "We have a perfect number!" : "It's not so perfect.");
+}
+perfectNumber(1)
