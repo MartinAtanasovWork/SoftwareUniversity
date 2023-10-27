5902a799ec16e0139c2acf3a46b2f52ee0cace49
MartinAtanasovWork <martiatanasof@gmail.com>
2023-10-27 18:52:04 +0300

    Create 01. Employees.js

---
+++ Fundamentals/Objects and Classes - Exercise/01. Employees.js
@@ -0,0 +1,13 @@
+function employees(arr){
+    for(let info of arr){
+        let employee = {name : info,pn : info.length}
+        console.log(`Name ${employee.name} -- Personal Number: ${employee.pn}`);
+    }
+}
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    )
