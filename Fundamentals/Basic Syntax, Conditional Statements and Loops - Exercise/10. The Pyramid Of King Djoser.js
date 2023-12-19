514167f5a24bded592cddaf02720803d7e6232a1
MartinAtanasovWork <martiatanasof@gmail.com>
2023-12-19 16:55:48 +0200

    Update 10. The Pyramid Of King Djoser.js

--- Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/10. The Pyramid Of King Djoser.js
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/10. The Pyramid Of King Djoser.js
@@ -5,24 +5,26 @@ function solve(base,increment){
     let gold =0;
     let heigth = 1;
     while(base > 2){
-        stone += (base - 2) ** 2;
-
-        if (heigth % 5 === 0) {
-            lapis += base * 2 + (base - 2) * 2;
-        } else {
+       
+        if(heigth % 5 == 0){
+            stone += (base - 2) ** 2;
+            lapis += base * 2 + (base -2) *2;
+        }else{
+            stone += (base - 2) ** 2;
             marble += base * 2 + (base - 2) * 2;
         }
         base -=2;
         heigth++;
     }
     if(base == 1){
-        gold = increment;
+        gold =1;
     }else if(base == 2){
-        gold = 4 * increment;
+        gold = 4;
     }
     stone *= increment;
     marble *= increment;
     lapis *= increment;
+    gold *= increment;
     heigth *= increment;
     console.log("Stone required:",stone.toFixed(0));
     console.log("Marble required:",marble.toFixed(0));
@@ -31,5 +33,6 @@ function solve(base,increment){
     console.log("Final pyramid height:",Math.floor(heigth));
 }
 solve(23,
-    0.5   
+    0.5
+   
     )
